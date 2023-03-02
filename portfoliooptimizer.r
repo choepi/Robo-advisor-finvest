ed2582175ab6d3360acb17c7b7899df2de34ee5c
Berger Patrik (berpat01) <berpat01@students.zhaw.ch>
2023-03-02 09:59:43 +0100

    -

--- portfoliooptimizer.r
+++ portfoliooptimizer.r
@@ -3,8 +3,9 @@ library(quantmod)
 library(rsconnect)
 rsconnect::setAccountInfo(name='fusionfinance',
                           token='7F40E0665C6370082775CB421F2570BF',
-                          secret='qieZeYAIhQpl8uE8+zwjI78xAoDQdXEMIwjWoBsI')
-
+                     
+                          
+                          
 #----
 ui <- fluidPage(
   titlePanel("Markowitz Portfolio Optimization"),
