c0a150fd9a5595409d3afd2f8aa9dd11950b4457
Dongtten <dongtten@students.zhaw.ch>
2023-03-16 16:02:13 +0100

    -

--- global.r
+++ global.r
@@ -1,24 +1,38 @@
 library(quantmod)
+library(zoo)
+library(xts)
+library(dplyr)
+
 
 pull <- function(x){
   a <- getSymbols(x,src='yahoo',auto.assign=FALSE)
-  a <- na.approx(a)
+  a <- fortify.zoo(a)
   return(a)
 }
+
 get_data <- function(n){
-  assets_list <- c("GC=F","BTC-USD","^GSPC","UTEN","CHF=X")
+  assets_list <- c("^SSMI","0VPY.L","GC=F","BTC-USD","^GSPC","^TNX","CHF=X")
+  asl <- c("SMI","SWIBND","GOLD","BITCOIN","SNP500","USBND","USDCHF")
+  st <- Sys.Date()
+  en <- st-n
+  #en <- as.character(en)
   #n = number of days as history
-  l <- matrix(nrow=n,ncol=length(assets_list))
-  nrow(l)
   for (i in 1:length(assets_list)){
-    r <- get_data(assets_list[i])
-    r <- rev(r)
-    r <- r[,4]
-    r <- r[1:n]
-    l[,i] <- r
+    r <- pull(assets_list[i])
+    # transpose of dataframe
+    transpose <- t(r)
+    transpose <- as.data.frame(transpose)
+    rev_data_frame <- rev(transpose)
+    rev_data_frame <- t(rev_data_frame)
+    r <- as.data.frame(rev_data_frame)
+    #r[,1] <- as.Date(r[,1])
+    e <- nrow(r %>% filter(r[,1] > en))
+    s <- r[1:e,c(1,5)]
+    assign(asl[i], s, envir = .GlobalEnv)
   }
-  return(l)
 }
 
-get_data(10)
-l
+get_data(1000)
+
+
+
