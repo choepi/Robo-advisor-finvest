1369beb6cf0460de5ff231d4bccd02debdfe8ee2
Dongtten <dongtten@students.zhaw.ch>
2023-03-22 19:44:42 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,3 +1,31 @@
+Skip to content
+ Enterprise
+Search or jump to…
+Pull requests
+Issues
+Explore
+ 
+@dongtten 
+dongtten
+/
+FusionFinance
+Private
+Code
+Issues
+Pull requests
+Projects
+Wiki
+Security
+Insights
+Settings
+FusionFinance/ui.R
+@dongtten
+dongtten -
+Latest commit b3cc795 23 minutes ago
+ History
+ 1 contributor
+84 lines (79 sloc)  2.65 KB
+
 library(shinydashboard)
 library(shiny)
 library(dplyr)
@@ -20,43 +48,78 @@ library(timevis)
 ## build ui.R -----------------------------------
 ## 1. header -------------------------------
 ui <- fluidPage(
-  titlePanel(title=div(img(src="fusion.jpg", height=100)
-                       ,"Portfolio Optimizing Tool")),
-  sidebarLayout(
-    sidebarPanel(h1("Willkommen"),
-                 fluidRow(
-                   column(8,
-                          selectInput("select1", h3("Menü"),
-                                       choices = list("Home" = 1, "Optimierung" = 2,
-                                                      "Forecast" = 3),selected = 1),
-                          br(),
-                          br(), 
-                          actionButton("action1", "Help"),
-                          ))
-                 ),
-    mainPanel(
-      h1("BLA BLA"),
-      h2("Kurse von Heute "),
-      fluidRow(
-        column(9,
-               sliderInput("slider1", h3("Risikolevel %"),
-                           min = 0, max = 100, value = 50),
-               br(),
-               br(),
-               dateInput("dateinput1", 
-                         h3("Auswahl Historie bis"), 
-                         value = "2020-01-01"),
-               br(), 
-               selectInput("select2", h3("SMI"),
-                           choices = list("SMI" ="SMI","SWIBND" = "SWIBND",
-                                          "GOLD"="GOLD","BITCOIN"="BITCOIN",
-                                          "SNP500"="SNP500","USBND"="USBND",
-                                          "USDCHF"="USDCHF"),selected = "SMI"),
-               br(), 
-               submitButton("Submit")
-        )),
-      textOutput("selected_var"),
-      plotOutput("historical_data")
+  header <- dashboardHeader(title=div(img(src="fusion.jpg", height=40)
+  )),
+  
+  sidebar <- dashboardSidebar(
+    sidebarMenu(
+      menuItem("Profil", tabName = "profil", icon = icon("user")),
+      menuItem("Portfolio", tabName = "portfolio", icon = icon("folder-open")),
+      menuItem("Kurse", tabName = "kurse", icon = icon("eye")))),
+  
+  body <- dashboardBody( tabItems(
+    tabItem(tabName = "profil",
+            h1("Profil"),
+            h5("Hier können sie ihr Portfolio eingeben oder erstellen lassen"),
+            mainPanel(tabsetPanel(
+              id = "tabsetPanelID",
+              type = "tabs",
+              tabPanel("Bestehendes Portfolio"),
+              tabPanel("Kein Portfolio"),
+            ))
+    ),
+    tabItem(tabName = "portfolio",
+            h1("Portfolio"),
+            h5("Einsehbarkeit der Performance des Portfolios anhand mvp oder tangential Methode"),
+            mainPanel(tabsetPanel(
+              id = "tabsetPanelID",
+              type = "tabs",
+              tabPanel("Historie",
+                       sliderInput("slider1", h3("Risikolevel %"),
+                                   min = 0, max = 100, value = 50)
+              ),
+              tabPanel("MVP"),
+              tabPanel("Tangential")
+            ))
+    ),
+    tabItem(tabName = "kurse",
+            h1("Kurse"),
+            h5("Aktuelle Kursangaben"),
+            fluidRow(
+              column(9,
+                     dateInput("dateinput1", 
+                               h3("Auswahl Historie bis"), 
+                               value = "2020-01-01"),
+                     br(), 
+                     selectInput("select2", h3("SMI"),
+                                 choices = list("SMI" ="SMI","SWIBND" = "SWIBND",
+                                                "GOLD"="GOLD","BITCOIN"="BITCOIN",
+                                                "SNP500"="SNP500","USBND"="USBND",
+                                                "USDCHF"="USDCHF"),selected = "SMI"),
+                     br(), 
+                     submitButton("Submit")
+              ),
+              column(9,
+                     textOutput("selected_var"),
+                     plotOutput("historical_data")
+              )),
+            
     )
   )
+  ),
+  
 )
+dashboardPage(header, sidebar, body)
+FooterZHAW
+ZHAW
+ZHAW
+© 2023 GitHub, Inc.
+Footer navigation
+Help
+Support
+API
+Training
+Blog
+About
+GitHub Enterprise Server 3.7.7
+You have no unread notifications
