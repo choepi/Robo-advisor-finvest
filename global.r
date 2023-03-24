21d98aa499d9278695dede4d513187b4845425cc
Dongtten <dongtten@students.zhaw.ch>
2023-03-25 00:59:09 +0100

    -

--- global.r
+++ global.r
@@ -2,13 +2,14 @@ library(quantmod)
 library(zoo)
 library(xts)
 library(dplyr)
+library(expm)
+library(imputeTS)
 
-
-get_data <- function(n){
+get_data <- function(){
   assets_list <- c("^SSMI","CSBGC0.SW","GC=F","BTC-USD","^GSPC","^TNX","CHF=X")
   asl <- c("SMI","SWIBND","GOLD","BITCOIN","SNP500","USBND","USDCHF")
   end <- Sys.Date()
-  start <- as.Date(end-n)
+  #start <- as.Date(end-n)
   l <- list(rep(NA,length(assets_list)))
   Stocks <- lapply(assets_list, getSymbols, auto.assign = FALSE)
   Stocks <- setNames(Stocks, asl)
@@ -17,9 +18,47 @@ get_data <- function(n){
     #r <- window(r, start = end, end=start)
     #r <- r[,4]
     colnames(r) <- c("Open","High","Low","Close","Volume","Adjusted")
-    l[[i]] <- r
+    l[[i]] <- na.omit(r)
   }
   return(l)
+}
+
+cbind.fill <- function(...){
+  nm <- list(...) 
+  nm <- lapply(nm, as.matrix)
+  n <- max(sapply(nm, nrow)) 
+  do.call(cbind, lapply(nm, function (x) 
+    rbind(x, matrix(, n-nrow(x), ncol(x))))) 
+}
+
+
+
+mvp <- function(wa){
+  zeithorizont = 365*2
+  var_m <- data.frame()
+  asl <- c("SMI","SWIBND","GOLD","BITCOIN","SNP500","USBND","USDCHF")
+  for (i in 1:7){
+    x <- (dat_asset[[i]])$Close
+    x <- x[1:zeithorizont]
+    var_m <- cbind.fill(var_m,x$Close)
+  }
+  
+  colnames(var_m) <- asl
+  var_m <- na_interpolation(var_m, option = "linear")
+  
+  #m <- matrix(diag(as.vector(var_m)), ncol = sum(wa));m#unkorrelierte assets
+  m <- cov(var_m)
+  #m <- rbind(c(0.04,0.01,0),c(0.01,0.05,0),c(0,0,0.02))
+  m <- round(m,3) 
+  m_inv <- round(solve(m),3)
+  
+  #korreliert?
+  #m <- cor(m)
+  #diag(m) <- var_m
+  
+  s <- (1/(t(rep(1,ncol(m)))%*%m%*%rep(1,ncol(m))))%*%rep(1,ncol(m))%*%m_inv
+  w <- round(s/sum(s),3);w
+  return(w)
 }
 
 
