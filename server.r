09127631660973b8e80f7330911f0a8e5f107b5e
Dongtten <dongtten@students.zhaw.ch>
2023-04-08 12:43:24 +0200

    -

--- server.r
+++ server.r
@@ -12,86 +12,132 @@ server <- function(input, output, session) {
   #data initialization
   assets_list <<- c("^SSMI","CSBGC0.SW","GC=F","BTC-USD","^GSPC","^TNX","CHF=X")
   asl <<- c("SMI","SWIBND","GOLD","BITCOIN","SNP500","USBND","USDCHF")
-  dat_asset <<- get_data() 
-  time_now <- Sys.time()
-  portfolio_s <<- c(1,0,0,0,0,0,0)
-  portfolio_s2 <<- c(1,0,0,0,0,0,0)
+  dat_asset <<- readRDS("database.RDS") #database einlesen
+  time_now <- Sys.Date()
+  #database updaten falls älter als 1,
+  if ((time_now - last(index(dat_asset[[4]]))) >= 1) {
+    dat_asset <<- get_data()
+    saveRDS(dat_asset, file = "database.RDS")
+  }
+  portfolio_s <<- c(1, 0, 1, 0, 0, 0, 0)
+  portfolio_s2 <<- c(1, 0, 0, 0, 0, 0, 0)
+  portfolio_w <<- 0
   
-  
-  hintjs(session, options = list("hintButtonLabel"="Hope this hint was helpful"),
-         events = list("onhintclose"=I('alert("Wasn\'t that hint helpful")')))
-  
-  observeEvent(input$help,
-               introjs(session, options = list("showBullets"="false", "showProgress"="true", 
-                                               "showStepNumbers"="false","nextLabel"="Next","prevLabel"="Prev","skipLabel"="Skip"))
+  #info serverfunktion
+  hintjs(
+    session,
+    options = list("hintButtonLabel" = "Hope this hint was helpful"),
+    events = list("onhintclose" = I('alert("Wasn\'t that hint helpful")'))
   )
   
   
   output$portfolio1 <- renderPlot({
-    for (i in 1:length(portfolio_s)){
+    for (i in 1:length(portfolio_s)) {
       portfolio_s[i] <<- input[[paste0("num", as.character(i))]]
     }
+    for (i in 1:length(asl)) {
+      portfolio_w[i] <- portfolio_s[i] * last(dat_asset[[i]]$Close)
+    }
+    portfolio_w <- round((portfolio_w), 0)
     
-    dat_v <- as.matrix(t(portfolio_s))
+    dat_v <- as.matrix(t(portfolio_w))
     colnames(dat_v) <- asl
-    dat_port <- data.frame(
-      group=colnames(dat_v),
-      value=c(dat_v)
-    )
-    ggplot(dat_port, aes(x="", y=value, fill=group)) +
-      geom_bar(stat="identity", width=1, color="white") +
-      coord_polar("y", start=0) +
+    dat_port <- data.frame(group = colnames(dat_v),
+                           value = c(dat_v))
+    ggplot(dat_port, aes(x = "", y = value, fill = group)) +
+      geom_bar(stat = "identity",
+               width = 1,
+               color = "white") +
+      coord_polar("y", start = 0) +
       theme_void() # remove background, grid, numeric labels
   })
-  
   
   
   output$portfolio2 <-  renderPlot({
-    for (i in 1:(length(portfolio_s2))){
-      portfolio_s2[i] <<- input[[paste0("num", as.character(i+7))]]
+    for (i in 1:(length(portfolio_s2))) {
+      portfolio_s2[i] <<- if (input[[paste0("checkbox", as.character(i))]]) 1
     }
+    
+    if (input$slider3 == "Geringes Risiko")
+      risk <<- 1
+    else if (input$slider3 == "Mittleres Risiko")
+      risk <<- 2
+    else if (input$slider3 == "Hohes Risiko")
+      risk <<- 3
+    
+    zu_invest_verm <<- input$num15
     
     dat_v <- as.matrix(t(portfolio_s2))
     colnames(dat_v) <- asl
-    dat_port <- data.frame(
-      group=colnames(dat_v),
-      value=c(dat_v)
-    )
-    ggplot(dat_port, aes(x="", y=value, fill=group)) +
-      geom_bar(stat="identity", width=1, color="white") +
-      coord_polar("y", start=0) +
+    dat_port <- data.frame(group = colnames(dat_v),
+                           value = c(dat_v))
+    ggplot(dat_port, aes(x = "", y = value, fill = group)) +
+      geom_bar(stat = "identity",
+               width = 1) +
+      coord_polar("y", start = 0) +
       theme_void() # remove background, grid, numeric labels
   })
   
   
-  
   output$portfolio_worth1 <- renderText({
-    #reactive auf inputs 
-    portfolio_w<<- portfolio_s
-    for (i in 1:length(portfolio_s)){
+    #reactive auf inputs
+    portfolio_w <<- portfolio_s
+    for (i in 1:length(portfolio_s)) {
       input[[paste0("num", as.character(i))]]
     }
     
-    for (i in 1:length(asl)){
-      portfolio_w[i] <- portfolio_s[i]*last(dat_asset[[i]]$Close)
+    for (i in 1:length(asl)) {
+      portfolio_w[i] <- portfolio_s[i] * last(dat_asset[[i]]$Close)
     }
-    w <- round(sum(portfolio_w),0)
-    paste("Portfolio Value:",w , "$")
+    w <- round(sum(portfolio_w), 0)
+    paste("Portfolio Value:", w , "$")
   })
   
   
   output$mvp <- renderPlot({
-    dat_v <- mvp(portfolio)
-    dat_mvp <- data.frame(
-      group=colnames(dat_v),
-      value=c(dat_v)
-    )
-    ggplot(dat_mvp, aes(x="", y=value, fill=group)) +
-      geom_bar(stat="identity", width=1, color="white") +
-      coord_polar("y", start=0) +
+    for (i in 1:length(portfolio_s)) {
+      input[[paste0("num", as.character(i))]]
+    }
+    a <- data.frame()
+    for (i in 1:length(portfolio_s)) {
+      if (portfolio_s[i] > 0)
+        a <- cbind.fill(a, dat_asset[[i]][,7])
+    }
+    dat_v <- mvp(a)
+    
+    dat_mvp <- data.frame(group = rownames(dat_v),
+                          value = c(dat_v))
+    ggplot(dat_mvp, aes(x = "", y = value, fill = group)) +
+      geom_bar(stat = "identity",
+               width = 1,
+               color = "white") +
+      coord_polar("y", start = 0) +
       theme_void()
   })
   
+  
+  output$tp <- renderPlot({
+    for (i in 1:length(portfolio_s)) {
+      input[[paste0("num", as.character(i))]]
+    }
+    a <- data.frame()
+    for (i in 1:length(portfolio_s)) {
+      if (portfolio_s[i] > 0)
+        a <- cbind.fill(a, dat_asset[[i]][,7])
+    }
+    
+    riskfree <<- 0.01
+    dat_v <- tp(a)
+    
+    dat_tp <- data.frame(group = rownames(dat_v),
+                         value = c(dat_v))
+    ggplot(dat_tp, aes(x = "", y = value, fill = group)) +
+      geom_bar(stat = "identity",
+               width = 1,
+               color = "white") +
+      coord_polar("y", start = 0) +
+      theme_void()
+  })
   
   
   output$selected_var <- renderText({ 
@@ -99,37 +145,48 @@ server <- function(input, output, session) {
   })
   
   
-  
   output$historical_data <- renderPlot({
-    if (input$slider2=="1D") a <- 1 #2d da am sonntag 1tag == 0
-    if (input$slider2=="5D") a <- 5
-    if (input$slider2=="1M") a <- 30
-    if (input$slider2=="6M") a <- 180
-    if (input$slider2=="1Y") a <- 365
-    if (input$slider2=="5Y") a <- 5*365
-    if (input$slider2=="Max.") a <- 0
+    if (input$slider2 == 1)
+      a <- 1 #2d da am sonntag 1tag == 0
+    else if (input$slider2 == 2)
+      a <- 5
+    else if (input$slider2 == 3)
+      a <- 30
+    else if (input$slider2 == 4)
+      a <- 180
+    else if (input$slider2 == 5)
+      a <- 365
+    else if (input$slider2 == 6)
+      a <- 5 * 365
+    else if (input$slider2 == 7)
+      a <- 0
     
-    if (abs(time_now-Sys.time())>300) dat_asset <<- get_data() #refresh nach 300s
+    
     
     chose <<- as.numeric(input$select2)
     dat <- as.xts(dat_asset[[chose]])
     start = last(index(dat))
-    if (a == 0) dat <- window(dat, start = first(index(dat)), end=start)
-    else if (a == 1 ) dat <- window(dat, start = start, end=start)
-    else dat <- window(dat, start = start-a, end=start)
+    if (a == 0)
+      dat <- window(dat, start = first(index(dat)), end = start)
+    else if (a == 1)
+      dat <- window(dat, start = start, end = start)
+    else
+      dat <- window(dat, start = start - a, end = start)
     
     if (input$radio1 == 1 & a == 1) {
-      ggplot(data = dat$Close, aes(x = Index, y = Close))+
+      ggplot(data = dat$Close, aes(x = Index, y = Close)) +
         geom_point()
     }
-    else if (input$radio1 == 1 & a != 1){
-      ggplot(data = dat$Close, aes(x = Index, y = Close))+
+    else if (input$radio1 == 1 & a != 1) {
+      ggplot(data = dat$Close, aes(x = Index, y = Close)) +
         geom_line()
     }
-    else if (input$radio1 == 2){
-      chartSeries(dat,name=asl[chose],theme = 'white')
+    else if (input$radio1 == 2) {
+      chartSeries(dat, name = asl[chose], theme = 'white')
     }
   })
+  
+  
   #Plot Protfolio time series
   output$weighted.portfolio <- renderPlot({
     if (input$sliderHistorie=="1D") b <- 1 #2d da am sonntag 1tag == 0
--- ui.r
+++ ui.r
@@ -51,46 +51,49 @@ ui <- dashboardPage(
                             Portfolio mit dem MVP oder dem Tangentialportfolio vergleichen kannst.
                             Selbstverständlich dürfen Sie auch willkürliche Gewichtungen eintragen, um ein Gefühl für
                             verschieden Assets zu erhalten."),
-                         introBox(
-                           column(2,
-                                  numericInput("num1", label = h5("SMI"), value = 1, width = 100, min = 0),
-                                  numericInput("num2", label = h5("SWIBND"), value = 1, width = 100, min = 0),
-                                  numericInput("num3", label = h5("GOLD"), value = 0, width = 100, min = 0),
-                                  numericInput("num4", label = h5("BITCOIN"), value = 0, width = 100, min = 0)),
-                           column(2,
-                                  numericInput("num5", label = h5("SNP500"), value = 0, width = 100, min = 0),
-                                  numericInput("num6", label = h5("USBND"), value = 0, width = 100, min = 0),
-                                  numericInput("num7", label = h5("USDCHF"), value = 0, width = 100, min = 0)),
-                           data.step = 1,data.intro = "Auswahl welches zu Ihnen passt"),
-                         column(7,
-                                plotOutput("portfolio1"),
-                                introBox(h4(textOutput("portfolio_worth1")),data.step = 2,
-                                         data.intro = "Hier sehen sie den aktuellen Wert ihres Portfolios")),
-                ),
+                         fluidPage(
+                           sidebarLayout(fluid=T,
+                                         introBox(
+                                           sidebarPanel(
+                                             numericInput("num1", label = h5("SMI"), value = 1, width = 100, min = 0),
+                                             numericInput("num2", label = h5("SWIBND"), value = 0, width = 100, min = 0),
+                                             numericInput("num3", label = h5("GOLD"), value = 1, width = 100, min = 0),
+                                             numericInput("num4", label = h5("BITCOIN"), value = 0, width = 100, min = 0)),
+                                           sidebarPanel(
+                                             numericInput("num5", label = h5("SNP500"), value = 0, width = 100, min = 0),
+                                             numericInput("num6", label = h5("USBND"), value = 0, width = 100, min = 0),
+                                             numericInput("num7", label = h5("USDCHF"), value = 0, width = 100, min = 0)),
+                                           data.step = 1,data.intro = "Auswahl welches zu Ihnen passt"),
+                                         mainPanel(introBox(h4(textOutput("portfolio_worth1")),data.step = 2,
+                                                            data.intro = "Hier sehen sie den aktuellen Wert ihres Portfolios"),
+                                                   plotOutput("portfolio1")
+                                         ),
+                                         position = c("left", "right")),
+                         )),
                 tabPanel("Kein Portfolio",
-                         h4("Hier kannst du mittels deinen individuellen Wünschen eine Portfoliooempfehlung erhalten,
+                         fluidPage(
+                           h4("Hier kannst du mittels deinen individuellen Wünschen eine Portfoliooempfehlung erhalten,
                             welches du dann nach bedarf anpassen kannst."),
-                         br(),
-                         h4("Zu investierendes Vermögen:"),
-                         numericInput("num15", label = h6(""), value = 0, width = 100, min = 0),
-                         sliderTextInput(
-                           inputId = "slider3",
-                           label = "Risikobereitschaft",
-                           choices = c("Geringes Risiko", "Mittleres Risiko", "Hohes Risiko"),
-                           selected = "Geringes Risiko"
-                         ),
-                         column(2,
-                                numericInput("num8", label = h5("SMI"), value = 1, width = 100, min = 0),
-                                numericInput("num9", label = h5("SWIBND"), value = 1, width = 100, min = 0),
-                                numericInput("num10", label = h5("GOLD"), value = 0, width = 100, min = 0),
-                                numericInput("num11", label = h5("BITCOIN"), value = 0, width = 100, min = 0)),
-                         column(2,
-                                numericInput("num12", label = h5("SNP500"), value = 0, width = 100, min = 0),
-                                numericInput("num13", label = h5("USBND"), value = 0, width = 100, min = 0),
-                                numericInput("num14", label = h5("USDCHF"), value = 0, width = 100, min = 0)),
-                         column(7,
-                                plotOutput("portfolio2")
-                                
+                           br(),
+                           basicPage(
+                             h4("Zu investierendes Vermögen:"),
+                             numericInput("num15", label = h6(""), value = 0, width = 100, min = 0),
+                             sliderTextInput(
+                               inputId = "slider3",
+                               label = "Risikobereitschaft",
+                               choices = c("Geringes Risiko", "Mittleres Risiko", "Hohes Risiko"),
+                               selected = "Geringes Risiko"
+                             ),
+                             column(5,
+                                    checkboxInput("checkbox1", "SMI", value = F),
+                                    checkboxInput("checkbox2", "SWIBND", value = F),
+                                    checkboxInput("checkbox3", "GOLD", value = F),
+                                    checkboxInput("checkbox4", "BITCOIN", value = F)),
+                             column(5,
+                                    checkboxInput("checkbox5", "SNP500", value = F),
+                                    checkboxInput("checkbox6", "USBND", value = F),
+                                    checkboxInput("checkbox7", "USD", value = F))),
+                           
                          )),
                 
               ))
@@ -111,40 +114,39 @@ ui <- dashboardPage(
                          plotOutput("weighted.portfolio",width = 500)
                 ),
                 tabPanel("MVP",
-                         fluidRow(column(5,
-                                         plotOutput("mvp")))),
-                tabPanel("Tangential")
+                         fluidRow(mainPanel(
+                           plotOutput("mvp")))),
+                tabPanel("TP",
+                         fluidRow(mainPanel(
+                           plotOutput("tp"))))
               ))
       ),
       tabItem(tabName = "kurse",
               h1("Kurse"),
               h5("Aktuelle Kursangaben"),
-              fluidRow(
-                column(9,
-                       #sliderInput("slider2", h3("Zeit Horizont"),
-                       #           min = 1, max = 7, value = 4),
-                       #helpText("1D  5D   1M   6M  1Y   5J  Max."),
-                       sliderTextInput(
-                         inputId = "slider2",
-                         label = "Choice",
-                         choices = c("1D","5D","1M","6M","1Y","5Y","Max."),
-                         selected = "1D"
-                       ),
-                       br(), 
-                       selectInput("select2", h3("SMI"),
-                                   choices = list("SMI" =1,"SWIBND" = 2,
-                                                  "GOLD"=3,"BITCOIN"=4,
-                                                  "SNP500"=5,"USBND"=6,
-                                                  "USDCHF"=7),selected = "SMI"),
-                       
-                       br(), 
-                       radioButtons("radio1", h3("Ansicht"),
-                                    choices = list("Simpel" = 1, "Erweitert" = 2),
-                                    selected = 1),
+              fluidPage(
+                
+                sidebarPanel(
+                  sliderTextInput(
+                    inputId = "slider2",
+                    label = "Choice",
+                    choices = c("1D","5D","1M","6M","1Y","5Y","Max."),
+                    selected = "1D"
+                  ),
+                  br(), 
+                  selectInput("select2", h3("SMI"),
+                              choices = list("SMI" =1,"SWIBND" = 2,
+                                             "GOLD"=3,"BITCOIN"=4,
+                                             "SNP500"=5,"USBND"=6,
+                                             "USD"=7),selected = "SMI"),
+                  textOutput("selected_var"),
+                  br(), 
+                  radioButtons("radio1", h3("Ansicht"),
+                               choices = list("Simpel" = 1, "Erweitert" = 2),
+                               selected = 1)
                 ),
-                column(9,
-                       textOutput("selected_var"),
-                       plotOutput("historical_data",width = 500)
+                mainPanel(
+                  plotOutput("historical_data")
                 )),
               
       )
