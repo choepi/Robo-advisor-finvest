8366f499aff49054a2f8edc8fd0f0e1ab44b46d2
Dongtten <dongtten@students.zhaw.ch>
2023-04-13 06:49:16 +0200

    Update server.r

--- server.r
+++ server.r
@@ -12,132 +12,86 @@ server <- function(input, output, session) {
   #data initialization
   assets_list <<- c("^SSMI","CSBGC0.SW","GC=F","BTC-USD","^GSPC","^TNX","CHF=X")
   asl <<- c("SMI","SWIBND","GOLD","BITCOIN","SNP500","USBND","USDCHF")
-  dat_asset <<- readRDS("database.RDS") #database einlesen
-  time_now <- Sys.Date()
-  #database updaten falls älter als 1,
-  if ((time_now - as.Date(last(index(dat_asset[[4]])))) >= 1) {
-    dat_asset <<- get_data()
-    saveRDS(dat_asset, file = "database.RDS")
-  }
-  portfolio_s <<- c(1, 0, 1, 0, 0, 0, 0)
-  portfolio_s2 <<- c(1, 0, 0, 0, 0, 0, 0)
-  portfolio_w <<- 0
+  dat_asset <<- get_data() 
+  time_now <- Sys.time()
+  portfolio_s <<- c(1,0,0,0,0,0,0)
+  portfolio_s2 <<- c(1,0,0,0,0,0,0)
   
-  #info serverfunktion
-  hintjs(
-    session,
-    options = list("hintButtonLabel" = "Hope this hint was helpful"),
-    events = list("onhintclose" = I('alert("Wasn\'t that hint helpful")'))
+  
+  hintjs(session, options = list("hintButtonLabel"="Hope this hint was helpful"),
+         events = list("onhintclose"=I('alert("Wasn\'t that hint helpful")')))
+  
+  observeEvent(input$help,
+               introjs(session, options = list("showBullets"="false", "showProgress"="true", 
+                                               "showStepNumbers"="false","nextLabel"="Next","prevLabel"="Prev","skipLabel"="Skip"))
   )
   
   
   output$portfolio1 <- renderPlot({
-    for (i in 1:length(portfolio_s)) {
+    for (i in 1:length(portfolio_s)){
       portfolio_s[i] <<- input[[paste0("num", as.character(i))]]
     }
-    for (i in 1:length(asl)) {
-      portfolio_w[i] <- portfolio_s[i] * last(dat_asset[[i]]$Close)
-    }
-    portfolio_w <- round((portfolio_w), 0)
     
-    dat_v <- as.matrix(t(portfolio_w))
+    dat_v <- as.matrix(t(portfolio_s))
     colnames(dat_v) <- asl
-    dat_port <- data.frame(group = colnames(dat_v),
-                           value = c(dat_v))
-    ggplot(dat_port, aes(x = "", y = value, fill = group)) +
-      geom_bar(stat = "identity",
-               width = 1,
-               color = "white") +
-      coord_polar("y", start = 0) +
+    dat_port <- data.frame(
+      group=colnames(dat_v),
+      value=c(dat_v)
+    )
+    ggplot(dat_port, aes(x="", y=value, fill=group)) +
+      geom_bar(stat="identity", width=1, color="white") +
+      coord_polar("y", start=0) +
       theme_void() # remove background, grid, numeric labels
   })
+  
   
   
   output$portfolio2 <-  renderPlot({
-    for (i in 1:(length(portfolio_s2))) {
-      portfolio_s2[i] <<- if (input[[paste0("checkbox", as.character(i))]]) 1
+    for (i in 1:(length(portfolio_s2))){
+      portfolio_s2[i] <<- input[[paste0("num", as.character(i+7))]]
     }
-    
-    if (input$slider3 == "Geringes Risiko")
-      risk <<- 1
-    else if (input$slider3 == "Mittleres Risiko")
-      risk <<- 2
-    else if (input$slider3 == "Hohes Risiko")
-      risk <<- 3
-    
-    zu_invest_verm <<- input$num15
     
     dat_v <- as.matrix(t(portfolio_s2))
     colnames(dat_v) <- asl
-    dat_port <- data.frame(group = colnames(dat_v),
-                           value = c(dat_v))
-    ggplot(dat_port, aes(x = "", y = value, fill = group)) +
-      geom_bar(stat = "identity",
-               width = 1) +
-      coord_polar("y", start = 0) +
+    dat_port <- data.frame(
+      group=colnames(dat_v),
+      value=c(dat_v)
+    )
+    ggplot(dat_port, aes(x="", y=value, fill=group)) +
+      geom_bar(stat="identity", width=1, color="white") +
+      coord_polar("y", start=0) +
       theme_void() # remove background, grid, numeric labels
   })
   
   
+  
   output$portfolio_worth1 <- renderText({
-    #reactive auf inputs
-    portfolio_w <<- portfolio_s
-    for (i in 1:length(portfolio_s)) {
+    #reactive auf inputs 
+    portfolio_w<<- portfolio_s
+    for (i in 1:length(portfolio_s)){
       input[[paste0("num", as.character(i))]]
     }
     
-    for (i in 1:length(asl)) {
-      portfolio_w[i] <<- portfolio_s[i] * last(dat_asset[[i]]$Close)
+    for (i in 1:length(asl)){
+      portfolio_w[i] <- portfolio_s[i]*last(dat_asset[[i]]$Close)
     }
-    w <- round(sum(portfolio_w), 0)
-    paste("Portfolio Value:", w , "$")
+    w <- round(sum(portfolio_w),0)
+    paste("Portfolio Value:",w , "$")
   })
   
   
   output$mvp <- renderPlot({
-    for (i in 1:length(portfolio_s)) {
-      input[[paste0("num", as.character(i))]]
-    }
-    a <- data.frame()
-    for (i in 1:length(portfolio_s)) {
-      if (portfolio_s[i] > 0)
-        a <- cbind.fill(a, dat_asset[[i]][,7])
-    }
-    dat_v <- mvp(a)
-    
-    dat_mvp <<- data.frame(Asset = rownames(dat_v),
-                           Gewicht = c(dat_v))
-    ggplot(dat_mvp, aes(x = "", y = Gewicht, fill = Asset)) +
-      geom_bar(stat = "identity",
-               width = 1,
-               color = "white") +
-      coord_polar("y", start = 0) +
+    dat_v <- mvp(portfolio)
+    dat_mvp <- data.frame(
+      group=colnames(dat_v),
+      value=c(dat_v)
+    )
+    ggplot(dat_mvp, aes(x="", y=value, fill=group)) +
+      geom_bar(stat="identity", width=1, color="white") +
+      coord_polar("y", start=0) +
       theme_void()
   })
   
-  
-  output$tp <- renderPlot({
-    for (i in 1:length(portfolio_s)) {
-      input[[paste0("num", as.character(i))]]
-    }
-    a <- data.frame()
-    for (i in 1:length(portfolio_s)) {
-      if (portfolio_s[i] > 0)
-        a <- cbind.fill(a, dat_asset[[i]][,7])
-    }
-    
-    riskfree <<- 0.01
-    dat_v <- tp(a)
-    
-    dat_tp <<- data.frame(Asset = rownames(dat_v),
-                         Gewicht = c(dat_v))
-    ggplot(dat_tp, aes(x = "", y = Gewicht, fill = Asset)) +
-      geom_bar(stat = "identity",
-               width = 1,
-               color = "white") +
-      coord_polar("y", start = 0) +
-      theme_void()
-  })
   
   
   output$selected_var <- renderText({ 
@@ -145,50 +99,38 @@ server <- function(input, output, session) {
   })
   
   
+  
   output$historical_data <- renderPlot({
-    if (input$slider2 == "1D")
-      a <- 1 #2d da am sonntag 1tag == 0
-    if (input$slider2 == "5D")
-      a <- 5
-    if (input$slider2 == "1M")
-      a <- 30
-    if (input$slider2 == "6M")
-      a <- 180
-    if (input$slider2 == "1Y")
-      a <- 365
-    if (input$slider2 == "5Y")
-      a <- 5 * 365
-    if (input$slider2 == "Max.")
-      a <- 0
+    if (input$slider2=="1D") a <- 1 #2d da am sonntag 1tag == 0
+    if (input$slider2=="5D") a <- 5
+    if (input$slider2=="1M") a <- 30
+    if (input$slider2=="6M") a <- 180
+    if (input$slider2=="1Y") a <- 365
+    if (input$slider2=="5Y") a <- 5*365
+    if (input$slider2=="Max.") a <- 0
     
-    
+    if (abs(time_now-Sys.time())>300) dat_asset <<- get_data() #refresh nach 300s
     
     chose <<- as.numeric(input$select2)
     dat <- as.xts(dat_asset[[chose]])
-    start = as.Date(last(index(dat)))
-    if (a == 0)
-      dat <- window(dat, start = first(index(dat)), end = start)
-    else if (a == 1)
-      dat <- window(dat, start = start, end = start)
-    
-    else
-      dat <- window(dat, start = start - a, end = start)
+    start = last(index(dat))
+    if (a == 0) dat <- window(dat, start = first(index(dat)), end=start)
+    else if (a == 1 ) dat <- window(dat, start = start, end=start)
+    else dat <- window(dat, start = start-a, end=start)
     
     if (input$radio1 == 1 & a == 1) {
-      ggplot(data = dat$Close, aes(x = Index, y = Close)) +
+      ggplot(data = dat$Close, aes(x = Index, y = Close))+
         geom_point()
     }
-    else if (input$radio1 == 1 & a != 1) {
-      ggplot(data = dat$Close, aes(x = Index, y = Close)) +
+    else if (input$radio1 == 1 & a != 1){
+      ggplot(data = dat$Close, aes(x = Index, y = Close))+
         geom_line()
     }
-    else if (input$radio1 == 2) {
-      chartSeries(dat, name = asl[chose], theme = 'white')
+    else if (input$radio1 == 2){
+      chartSeries(dat,name=asl[chose],theme = 'white')
     }
   })
-  
-  
-  #Plot Protfolio time series
+  #Plot Protfolio time series/ history of weighted Portfolio
   output$weighted.portfolio <- renderPlot({
     if (input$sliderHistorie=="1D") b <- 1 #2d da am sonntag 1tag == 0
     if (input$sliderHistorie=="5D") b <- 5
@@ -202,65 +144,38 @@ server <- function(input, output, session) {
     for (i in 1:length(portfolio_s)){
       input[[paste0("num", as.character(i))]]
     }
+    
+    #weighted portfolio with just close for basic plot
     normed.weights <- portfolio_s/sum(portfolio_s)
-    weighted.portfolio <<- normed.weights[1]*dat_asset[[1]][,4]+
-      normed.weights[2]*dat_asset[[2]][,4]+
-      normed.weights[3]*dat_asset[[3]][,4]+
-      normed.weights[4]*dat_asset[[4]][,4]+
-      normed.weights[5]*dat_asset[[5]][,4]+
-      normed.weights[6]*dat_asset[[6]][,4]+
-      normed.weights[7]*dat_asset[[7]][,4]
+    weighted.portfolio <<- normed.weights[1]*dat_asset[[1]]+
+      normed.weights[2]*dat_asset[[2]]+
+      normed.weights[3]*dat_asset[[3]]+
+      normed.weights[4]*dat_asset[[4]]+
+      normed.weights[5]*dat_asset[[5]]+
+      normed.weights[6]*dat_asset[[6]]+
+      normed.weights[7]*dat_asset[[7]]
     
     #plot.xts(weighted.portfolio)
-    start = as.Date(last(index(weighted.portfolio)))
+    #in column 4 is "close" of the chosen asset
+    start = last(index(weighted.portfolio))
     if (b == 0) dat <- window(weighted.portfolio, start = first(index(weighted.portfolio)), end=start)
     else if (b == 1 ) weighted.portfolio <- window(weighted.portfolio, start = start, end=start)
     else weighted.portfolio <- window(weighted.portfolio, start = start-b, end=start)
     
-    if (b == 1) {
-      ggplot(data = weighted.portfolio, aes(x = Index, y = Close))+
+    if (input$radioHistorie == 1 & b == 1) {
+      ggplot(data = weighted.portfolio[,4], aes(x = Index, y = Close))+
         geom_point(color = "green4")
     }
-    else if (b != 1){
-      ggplot(data = weighted.portfolio, aes(x = Index, y = Close)) +
+    else if (input$radioHistorie == 1 & b != 1){
+      ggplot(data = weighted.portfolio[,4], aes(x = Index, y = Close)) +
         geom_line(color = "green4")
+    }
+    else if (input$radioHistorie == 2){
+      chartSeries(weighted.portfolio ,name="Historie",theme = 'white')
     }
 
   })
   
-  output$mvprec <- renderTable({
-    for (i in 1:length(portfolio_s)) {
-      input[[paste0("num", as.character(i))]]
-    }
-    dat_mvp_rec <- dat_mvp
-    dat_mvp_rec$Gewicht <- dat_mvp$Gewicht * 100
-    as.data.frame(dat_mvp_rec)
-    g <- c(portfolio_w)
-    g <- g[g != 0]
-    lp <- c(1:length(g))
-    g <- sum(g)
-    g <- g * dat_mvp[lp, 2]
-    dat_mvp_rec$Investiert <- g
-    dat_mvp_rec
-    
-  })
-  
-  
-  output$tprec <- renderTable({
-    for (i in 1:length(portfolio_s)) {
-      input[[paste0("num", as.character(i))]]
-    }
-    dat_tp_rec <- dat_tp
-    dat_tp_rec$Gewicht <- dat_tp$Gewicht * 100
-    as.data.frame(dat_tp_rec)
-    g <- c(portfolio_w)
-    g <- g[g != 0]
-    lp <- c(1:length(g))
-    g <- sum(g)
-    g <- g * dat_tp[lp, 2]
-    dat_tp_rec$Investiert <- g
-    dat_tp_rec
-  })
   
 }
 
