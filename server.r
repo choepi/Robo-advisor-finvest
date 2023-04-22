360f7767198aaf8c5bc83d569c044175bf3eee1c
Dongtten <dongtten@students.zhaw.ch>
2023-04-22 14:13:11 +0200

    Update server.r

--- server.r
+++ server.r
@@ -1,15 +1,5 @@
 server <- function(input, output, session) {
   
-  
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  
   #data initialization
   assets_list <<- c("^SSMI","CSBGC0.SW","GC=F","BTC-USD","^GSPC","^TNX")
   asl <<- c("SMI","SWIBND","GOLD","BITCOIN","SNP500","USBND")
