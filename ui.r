ada5f5dab234c9c22f9f1433f32f2c8e88263002
witl24 <witprluc@students.zhaw.ch>
2023-05-08 13:52:26 +0200

    add fullscreen map

--- ui.r
+++ ui.r
@@ -258,7 +258,7 @@ ui <- dashboardPage(
                 tabPanel("AssetInfo",
                          includeHTML("AssetInfo.html")),
                 tabPanel("Geografische Verteilung",
-                         tabPanel("Map", leafletOutput("map"))
+                         tabPanel("Map", leafletOutput("map", width = "1250px", height = "600px"))
                 ),
                 tabPanel("Portfoliotheorie",
                          includeHTML("Portfoliotheorie.html"))
